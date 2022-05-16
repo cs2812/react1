5aa592ff1b603ed3a3b199cd9b41d6500fa54aa4
cs2812 <cs786786786@gmail.com>
2022-05-16 20:16:59 +0530

    first-app

---
+++ src/components/Mobile.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+let Mobile=()=> {
+  return (
+    <div className='div' >
+            <h1>Mobile Operating System</h1>
+            <ul>
+                <li>Android</li>
+                <li>Blckberry</li>
+                <li>iphone</li>
+                <li>Windows Phone</li>
+            </ul>
+
+            <h1>Mobile Manufacturers</h1>
+            <ul>
+                <li className="squr">Samsung</li>
+                <li className="squr">HTC</li>
+                <li>Micromax</li>
+                <li className="circle" >Apple</li>
+            </ul>
+        </div>
+    )
+  
+}
+export default Mobile
